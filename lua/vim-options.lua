ae45a3c40910cb2255c6f71655bdf074a0cf756b
Hosea77S <61354684+Hosea77S@users.noreply.github.com>
2025-07-21 20:35:04 +0200

    Enabled code folding with treesitter

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,6 +11,22 @@ vim.cmd("set shiftwidth=4")
 -- UNRELATED: for some reason <C-n> in insert mode opens text suggestions?
 
 --------------------------------------------------------------------------------
+-- VISUAL SETTINGS
+vim.cmd("set nowrap")
+vim.cmd("set cursorline")
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 2
+vim.opt.foldnestmax = 4
+-- zR open all folds
+-- zM close all open folds
+-- za toggles the fold at the cursor
+
+
+--------------------------------------------------------------------------------
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
