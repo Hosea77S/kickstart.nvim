f82795516e54df04fa179ee16a01506ae4221d97
Hosea77S <61354684+Hosea77S@users.noreply.github.com>
2025-07-22 07:50:54 +0200

    Set unix mode fileformat

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.cmd("set fileformat=unix")
 
 --------------------------------------------------------------------------------
 -- TAB settings (using vim scripting in lua)
