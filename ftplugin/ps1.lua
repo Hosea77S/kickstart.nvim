0209ec6cb60770dc849e4b3263b38326c38bfb2e
Hosea77S <61354684+Hosea77S@users.noreply.github.com>
2025-10-12 23:47:37 +0200

    Made background color darker

---
+++ ftplugin/ps1.lua
@@ -0,0 +1,6 @@
+
+vim.opt_local.foldenable = true
+vim.opt_local.foldlevel = 99
+vim.opt_local.foldlevelstart = 0
+vim.opt_local.foldnestmax = 99
+vim.opt_local.foldmethod = "indent"
--- lua/plugins/theme-plugins.lua
+++ lua/plugins/theme-plugins.lua
@@ -4,6 +4,12 @@ return {
         lazy = false,
         -- priority = 1000,
         config = function()
+            require("tokyonight").setup({
+                style = "night",
+                on_colors = function(colors)
+                    colors.bg = "#000000"
+                end
+            })
             vim.cmd([[colorscheme tokyonight]])
         end,
         opts = {},
