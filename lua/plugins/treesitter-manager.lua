77993ace17d19a14a8dba8676fb4d56397ffaa07
Hosea77S <shphetla77@gmail.com>
2026-04-14 08:11:44 +0200

    Added vhdl and systemverilog to tree sitter parsers

--- lua/plugins/treesitter-manager.lua
+++ lua/plugins/treesitter-manager.lua
@@ -24,10 +24,10 @@ return{
         config = function()
             require("tree-sitter-manager").setup({
                  -- Default Options
-                 ensure_installed = {"python", "cpp"}, -- list of parsers to install at the start of a neovim session
+                 ensure_installed = {"python", "cpp", "vhdl", "systemverilog"},
                  -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
                  -- auto_install = false, -- if enabled, install missing parsers when editing a new file
-                 highlight = {"python", "cpp", "lua"},
+                 highlight = {"python", "cpp", "lua", "vhdl", "systemverilog"},
                  -- languages = {}, -- override or add new parser sources
                  -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
                  -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
